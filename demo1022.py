595314611f630cd2619808d2e0984da23f74357d
YUYI-XAIO <yuyixaio@gmail.com>
2019-10-23 16:34:40 +0800

    modified var

--- demo1022.py
+++ demo1022.py
@@ -7,10 +7,10 @@ print('西元年月日：' + str(time.strftime("%Y-%m-%d", time.localtime())))
 print('時分秒：' + str(time.strftime("%H:%M:%S", time.localtime())))
 print('明天：' + str(datetime.date.today() + datetime.timedelta(days=1)))
 print('前天：' + str(datetime.date.today() - datetime.timedelta(days=2)))
-delete_1 = datetime.datetime.now() - datetime.timedelta(hours=1)
-add_1 = datetime.datetime.now() + datetime.timedelta(hours=1)
-print('前一個小時：' + str(delete_1.strftime("%H:%S:%M")))
-print('下一個小時：'+ str(add_1.strftime("%H:%S:%M")))
+delete = datetime.datetime.now() - datetime.timedelta(hours=1)
+add = datetime.datetime.now() + datetime.timedelta(hours=1)
+print('前一個小時：' + str(delete.strftime("%H:%S:%M")))
+print('下一個小時：'+ str(add.strftime("%H:%S:%M")))
 
 
 
