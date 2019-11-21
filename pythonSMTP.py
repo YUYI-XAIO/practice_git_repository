0679fee9ab25b74eed3f5cdb0d0fd2fe4b6bcd12
YUYI-XAIO <yuyixaio@gmail.com>
2019-11-21 15:54:43 +0800

    123

--- pythonSMTP.py
+++ pythonSMTP.py
@@ -6,10 +6,11 @@ from email.header import Header
 from dotenv import load_dotenv
 load_dotenv()
 
+# 用讀檔的方式
+
 sender = 'xiaoClassmate who is the Greatest' # 送件者
 receivers = [
-    'example_reciver_1@gmail.com',
-    'example_reciver_2@gmail.com'
+    'myg36t91@gmail.com'
 ] # 收件者
 subject = 'Python SMTP 的測試' # 主題
 body = '考試終於考了100分，..............，考多益' # 內容
@@ -24,7 +25,7 @@ server = smtplib.SMTP(host='smtp.gmail.com', port=587)
 try:
     server.ehlo()
     server.starttls(context=context)  # starttls 把已經存在的一條不安全的連結, 用SSL/TLS的加密方法, 升級成安全的連接
-    server.login('example_sender@gmail.com', password)
+    server.login('yuyixaio@gmail.com', password)
     server.sendmail(sender, receivers, msg.as_string())
     print('Success')
 except smtplib.SMTPException as E:
